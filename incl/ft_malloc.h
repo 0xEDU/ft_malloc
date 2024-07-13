dc20d091c4464014a2f37dcf079125caa8763878
edu <bfwaend@gmail.com>
2024-07-13 02:21:45 -0300

    Add macro for mmap function call

--- incl/ft_malloc.h
+++ incl/ft_malloc.h
@@ -1,6 +1,8 @@
 #ifndef FT_MALLOC_H
 #define FT_MALLOC_H
 
+#define MMAP(size) mmap(NULL, 1 * get_page_size(), PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, 0, 0)
+
 #include <stddef.h>
 #include <sys/mman.h>
 #include <unistd.h>

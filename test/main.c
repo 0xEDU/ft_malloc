88772ac593bd30c357f473b0d487a716ed3fd9b1
edu <bfwaend@gmail.com>
2024-07-13 19:50:07 -0300

    Test small allocation

--- test/main.c
+++ test/main.c
@@ -3,7 +3,12 @@
 #include <stddef.h>
 
 int main() {
-  void *ptr = malloc(42);
-  assert(ptr == NULL);
+  // Tiny allocation
+  void *tiny_ptr = malloc(42);
+  assert(tiny_ptr == NULL);
+  
+  // Small allocation
+  void *small_ptr = malloc(16442);
+  assert(small_ptr == NULL);
   return 0;
 }

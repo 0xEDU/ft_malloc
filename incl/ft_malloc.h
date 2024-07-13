b1e8f62fc67fbcb7ea5e6c7df640b033919ed78c
edu <bfwaend@gmail.com>
2024-07-13 03:14:39 -0300

    Add macros for malloc sizes

--- incl/ft_malloc.h
+++ incl/ft_malloc.h
@@ -1,8 +1,14 @@
 #ifndef FT_MALLOC_H
 #define FT_MALLOC_H
 
+// Macros
 #define MMAP(size) mmap(NULL, 1 * get_page_size(), PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANON, 0, 0)
+#define TINY_HEAP_ALLOCATION_SIZE (4 * get_page_size())
+#define TINY_BLOCK_SIZE (TINY_HEAP_ALLOCATION_SIZE / 128)
+#define SMALL_HEAP_ALLOCATION_SIZE (16 * get_page_size())
+#define SMALL_BLOCK_SIZE (SMALL_HEAP_ALLOCATION_SIZE / 128)
 
+// System includes
 #include <stddef.h>
 #include <sys/mman.h>
 #include <unistd.h>
@@ -13,7 +19,7 @@ void free(void *ptr);
 void *malloc(size_t size);
 void *realloc(void *ptr, size_t size);
 
-// Internals
+// Internals functions
 long get_page_size();
 
 #endif // !FT_MALLOC_H

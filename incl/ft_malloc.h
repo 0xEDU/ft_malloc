f5a9f5008e226d344e9af55d3f5e3c0ba3aadd14
edu <bfwaend@gmail.com>
2024-07-13 19:49:11 -0300

    Fix wrong MMAP call

--- incl/ft_malloc.h
+++ incl/ft_malloc.h
@@ -3,8 +3,7 @@
 
 // Macros
 #define MMAP(size)                                                             \
-  mmap(NULL, 1 * get_page_size(), PROT_READ | PROT_WRITE,                      \
-       MAP_PRIVATE | MAP_ANON, 0, 0)
+  mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, 0, 0)
 #define TINY_HEAP_ALLOCATION_SIZE (4 * get_page_size())
 #define TINY_BLOCK_SIZE (TINY_HEAP_ALLOCATION_SIZE / 128)
 #define SMALL_HEAP_ALLOCATION_SIZE (16 * get_page_size())
@@ -29,6 +28,7 @@ void *realloc(void *ptr, size_t size);
 long get_page_size();
 void try_allocate_zones(enum e_zone zone);
 
+// Global
 static t_zone_pointer_pool global_pointer_pool;
 
 #endif // !FT_MALLOC_H
